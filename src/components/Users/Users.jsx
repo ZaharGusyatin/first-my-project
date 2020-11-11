6c7ee2651626b5ad0450237c3e767b0b7863e60b
zaharko1996 <redimerlives@gmail.com>
2020-11-11 21:07:50 +0200

    API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,64 +1,22 @@
 import React from 'react'
 import s from "./Users.module.css";
+import * as axios from "axios";
+import userPhoto from '../../../src/assets/images/default-user-image.png'
 
 
 const Users = (props) => {
-    if (props.users.length===0){
-    props.setUsers([
-        {
-            id: 1,
-            followed: true,
-            photoUrl:'https://ua-news.liga.net/images/general/2019/09/11/20190911154809-5288.jpg',
-            fullName: 'Zahar1',
-            status: 'I Like It1',
-            location: {city: 'Cherkassy1', country: 'Ukraine1'}
-        },
-        {
-            id: 2,
-            followed: false,
-            photoUrl:'https://ua-news.liga.net/images/general/2019/09/11/20190911154809-5288.jpg',
-            fullName: 'Zahar2',
-            status: 'I Like It2',
-            location: {city: 'Cherkassy2', country: 'Ukraine2'}
-        },
-        {
-            id: 3,
-            followed: true,
-            photoUrl:'https://ua-news.liga.net/images/general/2019/09/11/20190911154809-5288.jpg',
-            fullName: 'Zahar3',
-            status: 'I Like It3',
-            location: {city: 'Cherkassy3', country: 'Ukraine3'}
-        },
-        {
-            id: 4,
-            followed: false,
-            photoUrl:'https://ua-news.liga.net/images/general/2019/09/11/20190911154809-5288.jpg',
-            fullName: 'Zahar4',
-            status: 'I Like It4',
-            location: {city: 'Cherkassy4', country: 'Ukraine4'}
-        },
-        {
-            id: 5,
-            followed: true,
-            photoUrl:'https://ua-news.liga.net/images/general/2019/09/11/20190911154809-5288.jpg',
-            fullName: 'Zahar5',
-            status: 'I Like It5',
-            location: {city: 'Cherkassy5', country: 'Ukraine5'}
-        },
-        {
-            id: 6,
-            followed: false,
-            photoUrl:'https://ua-news.liga.net/images/general/2019/09/11/20190911154809-5288.jpg',
-            fullName: 'Zahar6',
-            status: 'I Like It6',
-            location: {city: 'Cherkassy6', country: 'Ukraine6'}
-        }
-    ])}
+    if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            //отправляет на сервак
+            debugger
+            props.setUsers(response.data.items)
+        })
+    }
 
     return <div>{
         props.users.map(u => <div key={u.id}>
 
-            <span><dix><img src={u.photoUrl} className={s.userPhoto}/></dix>
+            <span><dix><img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto}/></dix>
                 <div>
                     {u.followed ?
                         <button onClick={() => {
@@ -74,12 +32,12 @@ const Users = (props) => {
             </span>
             <span>
             <span>
-                <div>{u.fullName}</div>
+                <div>{u.name}</div>
                 <div>{u.status}</div>
             </span>
          <span>
-             <div>{u.location.city}</div>
-             <div>{u.location.country}</div>
+             <div>{'u.location.city'}</div>
+             <div>{'u.location.country'}</div>
          </span>
         </span>
 
